d6ef6e343b3261c745630a7918299ddbf39df032
Matthew Huang <matthewhuang19@gmail.com>
2019-09-11 17:02:42 -0400

    [App] Add NotFound page

--- web/app/App.js
+++ web/app/App.js
@@ -4,6 +4,7 @@ import '~/styles/style.css';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import { useAuth } from '~/lib/auth';
 import LandingPage from './LandingPage';
+import NotFound from './NotFound';
 import SignupPage from './SignupPage';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Switch>
       <Route exact path={'/'} component={LandingPage} />
       <Route path={'/signup'} component={SignupPage} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
---
+++ web/app/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotFound() {
+  return <div className="text-red-500">Not found</div>;
+}
