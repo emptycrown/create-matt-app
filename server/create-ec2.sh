32c5f123560bace45d55c3ef6fe8d6cf8e9160de
Matthew Huang <matthewhuang19@gmail.com>
2019-09-17 14:39:43 -0700

    [Server script] chmod

--- server/create-ec2.sh
+++ server/create-ec2.sh
@@ -2,6 +2,12 @@
 # Sample script to initialize an EC2 app
 set -e
 
+if [ $(id -u) != "0" ]
+then
+    echo "FAILED: Run with sudo!"
+    exit
+fi
+
 sudo apt-get update
 sudo apt-get install -y nginx
 
@@ -11,18 +17,16 @@ sudo apt-get install -y nodejs
 # Yarn install
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn
+sudo apt-get update && sudo apt-get install -y yarn
 
 # Then setup SSL via Let's Encrypt
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository universe
 sudo add-apt-repository -y ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install -y certbot python-certbot-nginx
 # sudo certbot certonly --nginx
-
-# Then clone the repo + run it!
 
 # Other dependencies
 npm install -g pm2
